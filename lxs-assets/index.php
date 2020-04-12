9257e1d57a41db79a42980219306e55d76c72181
Lutfi Anam <63002253+lutfianam@users.noreply.github.com>
2020-04-12 19:03:00 +0700

    Add files via upload

--- lxs-assets/index.php
+++ lxs-assets/index.php
@@ -1,1 +1,4 @@
-
+<?php
+// Redirect Ke Blog Lutfi Anam ( Itung - Itung Buat Sedekah Ke Developer:V )
+echo '<script type="text/javascript">window.location = "http://wegihngetik.blogspot.com"</script>';
+?>
